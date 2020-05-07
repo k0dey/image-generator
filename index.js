a6816fd2ac4d9f8fd8b92f3f1ed733bada360a32
Kerry Parsons <kerryparsons1204@gmail.com>
2020-05-07 15:18:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const container = document.getElementById('container');
+
+const submitBtn = document.getElementById('submit');
+const clearBtn = document.getElementById('clear');
+
+const input = document.getElementById('boxNumber');
+const search = document.getElementById('searchTerm');
+
+
+
+submitBtn.onclick = newBox;
+clearBtn.onclick = clearBox;
+
+
+
+function imageGen(search) {
+    let numGen = Math.ceil((Math.random() * 1000) + 1);
+
+    return `<img src="https://source.unsplash.com/random/500x500/?sig=${numGen}&${search}" width="200px" height="200px">`
+}
+
+function newBox() {
+    for(i=0;i<input.value;i++){
+        const box = document.createElement('div');
+        box.classList.add("box");
+        box.innerHTML = `${imageGen(search.value)}`;
+        container.appendChild(box)
+    }
+
+}
+
+function clearBox() {
+    while (container.firstChild)
+{
+    container.removeChild(container.lastChild)
+}
+}
+
+
